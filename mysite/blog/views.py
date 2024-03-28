dd78a6909dc2cb404e937809b18f6c753604fb3c
scelo <scelogumede95@gmail.com>
2024-03-28 09:52:55 +0200

    with an incomplete form for emails

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator
+from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
-
-def post_list(request):
-    post_list = Post.published.all()
-    paginator=Paginator(post_list,3)
-
-    page_number=request.GET.get('page',1)
-    posts=paginator.page(page_number)
-
-    return render(request,
-                 'blog/post/list.html',
-                 {'posts': posts})
+class PostListView(ListView):
+    queryset=Post.published.all()
+    context_object_name='posts'
+    paginate_by=5
+    template_name='blog/post/list.html'
 
 
 def post_detail(request, year, month, day , post):
@@ -26,3 +22,20 @@ def post_detail(request, year, month, day , post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+def post_share(request, post_id):
+
+    post= get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    if request.method=='POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+
+            cd = form.cleaned_data
+
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post//share.html',{'post':post,
+                                                    'form':form})
